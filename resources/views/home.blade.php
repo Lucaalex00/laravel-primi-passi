ff2a15c47d7607e11e47416df0cadeaf7fb366ed
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-06 14:38:49 +0200

    add Data array & print in page Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
-</head>
-<body>
-    
-    <h1>Hello World</h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="../css/app.css">
+        <!-- PERCHE' IL PATH NON DOVREBBE ESSERE CORRETTO ??? -->
+        
+    </head>
+    <body>
+        
+        <h1 class="title">{{$message}} {{$name}} !!!</h1>
 
-</body>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data =[
+        'message' => 'Welcome',
+        'name' => 'USER',
+    ];
+    
+    return view('home', $data);
 });
